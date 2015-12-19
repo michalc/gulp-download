948435c110a75c30c4f0fba81c755735d075af74
Michal Charemza <michalcharemza@gmail.com>
2015-12-19 13:57:32 +0000

    Better error handling

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var pretty = require("pretty-hrtime");
 var merge = require("merge");
 var col = gutil.colors;
 var log = gutil.log;
+var Error = gutil.PluginError;
 
 module.exports = function(urls, options) {
   urls = typeof urls === 'string' ? [urls] : urls;
@@ -23,17 +24,38 @@ module.exports = function(urls, options) {
 
     var contents = through();
 
-    var start = process.hrtime();
-    log('Downloading', col.magenta(url) + '...');
+    function emitError(e) {
+      errored = true;
+      contents.emit('error', new Error('gulp-download-stream', e));
+    }
 
+    var start = process.hrtime();
+    var errored = false;
+    log('Downloading', col.magenta(url) + '...');
     var r = request(merge({
       url: url,
       encoding: null
     }, options))
-    .on('end', function() {
-      var end = process.hrtime(start);
-      log('Downloaded', col.magenta(url), 'after', col.magenta(pretty(end)));
-    }).pipe(contents);
+      .on('error', function(e) {
+        emitError(e);
+      })
+      .on('response', function(response) {
+        if (response.statusCode >= 400) {
+          emitError(col.magenta(response.statusCode) + ' returned from ' + col.magenta(url));
+        }
+      })
+      .on('end', function(e) {
+        if (!errored) {
+          var end = process.hrtime(start);
+          log('Downloaded', col.magenta(url), 'after', col.magenta(pretty(end)));
+        }
+      })
+      .pipe(contents)
+      .on('error', function() {
+        // Avoids "Unhandled stream error in pipe" messages.
+        // gulp will still fail the containing task if there is
+        // an error downloading
+      });
 
     return new gutil.File({
       path: fileName,
